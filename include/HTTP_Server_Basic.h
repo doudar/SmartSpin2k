dde3d75de4942fc3d1fda0e19ced1ec55aa5ad66
doudar <anthonydoud97@gmail.com>
2020-08-31 21:47:15 -0500

    Update HTTP_Server_Basic.h

--- include/HTTP_Server_Basic.h
+++ include/HTTP_Server_Basic.h
@@ -1,9 +1,4 @@
-/*
-ESP32 Web Server - AP Mode
-modified on 25 MAy 2019
-by Mohammadreza Akbari @ Electropeak
-https://electropeak.com/learn
-*/
+
 #ifndef HTTP_SERVER_BASIC_H
 #define HTTP_SERVER_BASIC_H
 
