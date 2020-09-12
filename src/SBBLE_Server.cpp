eaa45c6fe6dd79d3cec3c005cc03a06b5c72b8f3
Dustan D <41238223+cancellogic@users.noreply.github.com>
2020-09-12 09:26:51 -0500

    Update SBBLE_Server.cpp

--- src/SBBLE_Server.cpp
+++ src/SBBLE_Server.cpp
@@ -1,4 +1,9 @@
-
+// SmartSpin2K code
+// This software registers an ESP32 as a BLE FTMS device which then uses a stepper motor to turn the resistance knob on a regular spin bike.
+// BLE code based on examples from https://github.com/nkolban
+// Copyright 2020 Anthony Doud
+// This work is licensed under the GNU General Public License v2
+// Prototype hardware build from plans in the SmartSpin2k repository are licensed under Cern Open Hardware Licence version 2 Permissive
 
 /* Assioma Pedal Information for later
 BLE Advertised Device found: Name: ASSIOMA17287L, Address: e8:fe:6e:91:9f:16, appearance: 1156, manufacturer data: 640302018743, serviceUUID: 00001818-0000-1000-8000-00805f9b34fb
