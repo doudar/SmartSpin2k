0057a32957d8a79e3023a8ca7cb9f98c43db7b61
doudar <anthonydoud97@gmail.com>
2020-10-18 15:35:35 -0500

    debug cadence

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,7 +9,7 @@
 #include <Arduino.h>
 #include <SPIFFS.h>
 
-const String FirmwareVer = {"0.0.10.9"};
+const String FirmwareVer = {"0.0.10.10"};
 String debugToHTML = "<br>Current Firmware Version: " + FirmwareVer;
 
 bool lastDir = true; //Stepper Last Direction
