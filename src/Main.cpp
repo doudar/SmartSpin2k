89201f30f72a21682fe5234fd27cfb1a14e636fd
doudar <anthonydoud97@gmail.com>
2020-10-19 14:50:34 -0500

    Updated version

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,7 +9,7 @@
 #include <Arduino.h>
 #include <SPIFFS.h>
 
-const String FirmwareVer = {"0.0.10.11"};
+const String FirmwareVer = {"0.0.10.12"};
 String debugToHTML = "<br>Current Firmware Version: " + FirmwareVer;
 
 bool lastDir = true; //Stepper Last Direction
