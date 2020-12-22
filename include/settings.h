c3e75e81425d3e27aa74813475c7745f8bc7e0a0
doudar <anthonydoud97@gmail.com>
2020-12-22 16:44:58 -0600

    added better initial background color until css loads

--- include/settings.h
+++ include/settings.h
@@ -10,7 +10,7 @@
 #define SETTINGS_H
 
 //Current program version info. Used for auto updates
-#define FIRMWARE_VERSION "0.0.12.21"
+#define FIRMWARE_VERSION "0.0.12.22"
 
 //Update firmware on boot?
 #define AUTO_FIRMWARE_UPDATE true
