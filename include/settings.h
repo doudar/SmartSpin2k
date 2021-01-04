64751191282afc63f2e788387e458d01e8d4e6a1
doudar <anthonydoud97@gmail.com>
2021-01-03 23:32:50 -0600

    Version Update

--- include/settings.h
+++ include/settings.h
@@ -10,7 +10,7 @@
 #define SETTINGS_H
 
 //Current program version info. Used for auto updates
-#define FIRMWARE_VERSION "0.1.1.2"
+#define FIRMWARE_VERSION "0.1.1.3"
 
 //Update firmware on boot?
 #define AUTO_FIRMWARE_UPDATE true
