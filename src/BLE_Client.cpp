e31ebd83180e69f982bc536a0bd9a2ba98046fd4
doudar <anthonydoud97@gmail.com>
2021-02-20 13:58:02 -0600

    hr bugfix

--- src/BLE_Client.cpp
+++ src/BLE_Client.cpp
@@ -109,11 +109,14 @@ static void notifyCallback(
         //Set data from Fitness Machine Service 
     if (pBLERemoteCharacteristic->getUUID() == FITNESSMACHINEINDOORBIKEDATA_UUID)
     {
-            if(pData[2]>0)
+            byte flags = pData[0];
+            //if (bitRead(flags, 9)){
+            if(pData[8]>0)
             {
-                userConfig.setSimulatedHr(pData[2]);
-                debugDirector("Bike HR: " + String(pData[2]));
+                userConfig.setSimulatedHr(pData[8]);
+                debugDirector("Bike HR: " + String(pData[8]));
             }
+            //}
             userConfig.setSimulatedWatts(bytes_to_u16(pData[7], pData[6]));
             userConfig.setSimulatedCad((bytes_to_u16(pData[5], pData[4]))/2);
             debugDirector(" Indoor Bike Data: " + String(userConfig.getSimulatedWatts()) + "W " + String(userConfig.getSimulatedCad()) + " CAD");
