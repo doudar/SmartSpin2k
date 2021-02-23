0492d5a60963dccd2ce2362298caeea348bfef97
doudar <anthonydoud97@gmail.com>
2021-02-22 21:30:47 -0600

    Changed Flywheel UUID's

--- include/BLE_Common.h
+++ include/BLE_Common.h
@@ -36,9 +36,9 @@
 #define FITNESSMACHINEPOWERRANGE_UUID BLEUUID((uint16_t)0x2AD8)
 
 // GATT service/characteristic UUIDs for Flywheel Bike from ptx2/gymnasticon/
-#define FLYWHEEL_UART_SERVICE_UUID BLEUUID("6e400001b5a3f393e0a9e50e24dcca9e")
-#define FLYWHEEL_UART_RX_UUID BLEUUID("6e400002b5a3f393e0a9e50e24dcca9e")
-#define FLYWHEEL_UART_TX_UUID BLEUUID("6e400003b5a3f393e0a9e50e24dcca9e")
+#define FLYWHEEL_UART_SERVICE_UUID BLEUUID((uint16_t)0xCA9E)
+#define FLYWHEEL_UART_RX_UUID BLEUUID((uint16_t)0xCA9E)
+#define FLYWHEEL_UART_TX_UUID BLEUUID((uint16_t)0xCA9E)
 
 // macros to convert different types of bytes into int The naming here sucks and should be fixed.
 #define bytes_to_s16(MSB, LSB) (((signed int)((signed char)MSB))) << 8 | (((signed char)LSB))
