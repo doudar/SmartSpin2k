325e8d858f34cc50bda47758b8e886fdb8dd5629
Joel Baranick <jbaranick@gmail.com>
2021-02-24 02:43:29 -0800

    Get version from git

---
+++ git_tag_macro.py
@@ -0,0 +1,18 @@
+import subprocess
+
+branch = (
+    subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+    .strip()
+    .decode("utf-8")
+)
+tag = (
+    subprocess.check_output(["git", "describe", "--tags"])
+    .strip()
+    .decode("utf-8")
+)
+if branch != "master":
+    tag_parts = tag.split("-")
+    tag = "%s-%s" % branch
+    if len(tag_parts) > 1:
+        tag = "%s-%s" % (tag, "-".join(tag_parts[1:]))
+print("-DFIRMWARE_VERSION='\"%s\"'" % tag)
