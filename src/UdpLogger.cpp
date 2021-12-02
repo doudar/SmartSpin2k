f27935b0c140e9b3defc8186b59c728ab9b01289
doudar <anthonydoud97@gmail.com>
2021-12-01 20:44:42 -0600

    Added license

--- src/UdpLogger.cpp
+++ src/UdpLogger.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2020  Anthony Doud & Joel Baranick
+ * All rights reserved
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
 #include "UdpLogger.h"
 
 UdpLogger UdpLogger::INSTANCE = UdpLogger();
