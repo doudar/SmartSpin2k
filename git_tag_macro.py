5019aae3bbf4eb92e2b6a1d1302dccc5974b2f73
anthony <anthonydoud97@gmail.com>
2022-07-16 17:36:04 -0500

    Return last tag if run on push

--- git_tag_macro.py
+++ git_tag_macro.py
@@ -6,14 +6,25 @@ branch = (
     .strip()
     .decode("utf-8")
 )
-tag = (
+
+try:
+    tag = (
     subprocess.check_output(["git", "describe", "--tags"])
     .strip()
     .decode("utf-8")
-)
+    )
+
+except:
+    tag = (
+    subprocess.check_output(["git", "describe", "--abbrev=0" "--tags"])
+    .strip()
+    .decode("utf-8")
+    )
+
 if branch != "master":
     tag_parts = tag.split("-")
     tag = "%s-%s" % (tag_parts[0], branch)
     if len(tag_parts) > 1:
         tag = "%s-%s" % (tag, "-".join(tag_parts[1:]))
 print("-DFIRMWARE_VERSION='\"%s\"'" % tag)
+
