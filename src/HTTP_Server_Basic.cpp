b6a77f4d2efd04fc04fb72328e698babb03abdda
doudar <anthonydoud97@gmail.com>
2023-01-08 10:13:49 -0600

    removed html loading delays

--- src/HTTP_Server_Basic.cpp
+++ src/HTTP_Server_Basic.cpp
@@ -148,7 +148,7 @@ void HTTP_Server::start() {
     String response =
         "<!DOCTYPE html><html><body>Scanning for BLE Devices. Please wait "
         "15 seconds.</body><script> setTimeout(\"location.href = 'http://" +
-        myIP.toString() + "/bluetoothscanner.html';\",15000);</script></html>";
+        myIP.toString() + "/bluetoothscanner.html';\",10000);</script></html>";
     spinBLEClient.resetDevices();
     spinBLEClient.serverScan(true);
     server.send(200, "text/html", response);
@@ -590,16 +590,21 @@ void HTTP_Server::settingsProcessor() {
         "setTimeout(\"location.href = 'http://" +
         myIP.toString() + "/index.html';\",1000);</script></html>";
   }
-  server.send(200, "text/html", response);
   SS2K_LOG(HTTP_SERVER_LOG_TAG, "Config Updated From Web");
   userConfig.saveToLittleFS();
   userConfig.printFile();
   userPWC.saveToLittleFS();
   userPWC.printFile();
   if (reboot) {
+        response +=
+        "Please wait while your settings are saved and SmartSpin2k reboots.</h2></body><script> "
+        "setTimeout(\"location.href = 'http://" +
+        myIP.toString() + "/bluetoothscanner.html';\",5000);</script></html>";
+        server.send(200, "text/html", response);
     vTaskDelay(100 / portTICK_PERIOD_MS);
     ESP.restart();
   }
+  server.send(200, "text/html", response);
 }
 
 void HTTP_Server::stop() {
