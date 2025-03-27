8b8ec61efdc1d5957e68b27fb53ca902c2e22dd1
Anthony Doud <anthonydoud97@gmail.com>
2025-03-26 21:27:16 -0500

    Modified ERG Sensitivity

--- src/ERG_Mode.cpp
+++ src/ERG_Mode.cpp
@@ -227,7 +227,7 @@ void ErgMode::_setPointChangeState(int newCadence, Measurement& newWatts) {
 // PrevError
 void ErgMode::_inSetpointState(int newCadence, Measurement& newWatts) {
   // Setting Gains For PID Loop
-  float Kp = userConfig->getERGSensitivity() * 2;
+  float Kp = userConfig->getERGSensitivity();
   float Ki = 0.1;
   float Kd = 0.1;
 
