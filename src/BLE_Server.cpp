fdc0c3bdbfc982785def73b07272b5f9ff3bc109
Anthony Doud <anthonydoud97@gmail.com>
2026-03-16 13:36:54 -0500

    Update BLE_Server.cpp

--- src/BLE_Server.cpp
+++ src/BLE_Server.cpp
@@ -57,7 +57,7 @@ void startBLEServer() {
 
   // Zwift identifies controllers via manufacturer data with company ID 0x094A.
   // Set the last two bytes to the last two bytes of our BLE address in little endian.
-  uint8_t zwiftMfrData[] = {0x4A, 0x09, 0x0B, 0x58, 0x9A};
+  uint8_t zwiftMfrData[] = {0x4A, 0x09, 0x08, 0x58, 0x9A};
   const std::string bleAddress = BLEDevice::getAddress().toString();  // "aa:bb:cc:dd:ee:ff"
   unsigned int mac[6]          = {0};
   if (sscanf(bleAddress.c_str(), "%02x:%02x:%02x:%02x:%02x:%02x", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]) == 6) {
