33bcc8bc12f9abdb3a56e72a9e4af057ab50f75c
Anthony Doud <anthonydoud97@gmail.com>
2026-08-14 11:54:51 -0500

    GitHub Actions now explicitly sets the firmware version to the date-based release version

--- git_tag_macro.py
+++ git_tag_macro.py
@@ -1,31 +1,28 @@
+import os
 import subprocess
 
-subprocess.run(["git", "fetch", "--unshallow"])
-branch = (
-    subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
-    .strip()
-    .decode("utf-8")
-)
+tag = os.environ.get("SS2K_FIRMWARE_VERSION")
 
-#try:
-#    subprocess.check_call(["git", "describe", "--tags"])
-#except:
-#    tag = (
-#    subprocess.check_output(["git", "describe", "--abbrev=0" "--tags"])
-#    .strip()
-#    .decode("utf-8")
-#    )
-#else:
-tag = (
-    subprocess.check_output(["git", "describe", "--tags", "--always"])
-    .strip()
-    .decode("utf-8")
+if os.environ.get("GITHUB_ACTIONS") == "true" and not tag:
+    raise RuntimeError("SS2K_FIRMWARE_VERSION must be set for GitHub Actions builds")
+
+if not tag:
+    subprocess.run(["git", "fetch", "--unshallow"])
+    branch = (
+        subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
+        .strip()
+        .decode("utf-8")
     )
 
-if branch != "master":
-    tag_parts = tag.split("-")
-    tag = "%s-%s" % (tag_parts[0], branch)
-    if len(tag_parts) > 1:
-        tag = "%s-%s" % (tag, "-".join(tag_parts[1:]))
-print("-DFIRMWARE_VERSION='\"%s\"'" % tag)
+    tag = (
+        subprocess.check_output(["git", "describe", "--tags", "--always"])
+        .strip()
+        .decode("utf-8")
+    )
 
+    if branch != "master":
+        tag_parts = tag.split("-")
+        tag = "%s-%s" % (tag_parts[0], branch)
+        if len(tag_parts) > 1:
+            tag = "%s-%s" % (tag, "-".join(tag_parts[1:]))
+print("-DFIRMWARE_VERSION='\"%s\"'" % tag)
